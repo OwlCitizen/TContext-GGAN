d3d758ab1866a095b33e5cb98da88bc558542f83
OwlCitizen <59813345+OwlCitizen@users.noreply.github.com>
2022-03-14 13:43:51 +0800

    Add files via upload

---
+++ cols_eicu.py
@@ -0,0 +1,6 @@
+#cols.py
+medi=['metoprolol','atorvastatin','simvastatin','aspirin','clopidogrel']#,'captopril','bumetanide']
+inputs=['nitroglycerin','diltiazem','esmolol']#,'labetalol','verapamil','metoprolol_i']
+lab=['bp_systolic','bp_mean','heartrate','respiratory_rate','spo2','hemoglobin',
+      'glucose','body_temperature','fio2','sao2','alt','ast']#,'cholesterol','tg','tc','ldl','hdl','ck','tni','tnt']
+cols=lab+inputs+medi
